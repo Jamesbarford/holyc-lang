b7440dbf126e3c7e89951079f3a3a680e8666ad2
Jamesbarford <jamesbarfordevans@gmail.com>
2026-05-15 20:08:20 +0100

    Release - beta-v0.0.11 & update README

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION__
 #define VERSION__
 
-#define HCC_VERSION ("beta-v0.0.10")
+#define HCC_VERSION ("beta-v0.0.11")
 
 static const char *cctrlGetVersion(void) {
     return HCC_VERSION;
