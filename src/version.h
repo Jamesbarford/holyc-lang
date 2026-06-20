9108b944927a0197135e3894955cac2a211dccf3
James <jamesbarfordevans@gmail.com>
2026-06-20 10:05:43 +0100

    v0.0.13-beta (#249)

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION__
 #define VERSION__
 
-#define HCC_VERSION ("beta-v0.0.12")
+#define HCC_VERSION ("v0.0.13-beta")
 
 static const char *cctrlGetVersion(void) {
     return HCC_VERSION;
